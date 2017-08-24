580b096987efc9d79ada41f84f3f5020478a230e
BugDiver <vinaysh@thoughtworks.com>
2017-08-24 20:06:12 +0530

    Added travis.yml.

---
+++ setup.sh
@@ -0,0 +1,8 @@
+installPackages (){
+    curl http://s.sudre.free.fr/Software/files/Packages.dmg -o packages.dmg
+    sudo hdiutil attach ./packages.dmg
+    sudo installer -package "/Volumes/Packages\ 1.1.3/packages/Packages.pkg" -target /
+    sudo hdiutil detach "/Volumes/Packages 1.1.3"
+}
+
+installPackages
